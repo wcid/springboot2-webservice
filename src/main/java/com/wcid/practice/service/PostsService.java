c7e16d04fb22eabd5b1f1039ed3dd9d0eff37799
김지한 <jihankim@crowdworks.kr>
2020-08-16 22:22:19 +0900

    CRUD 구현

--- src/main/java/com/wcid/practice/service/PostsService.java
+++ src/main/java/com/wcid/practice/service/PostsService.java
@@ -2,12 +2,16 @@ package com.wcid.practice.service;
 
 import com.wcid.practice.domain.posts.Posts;
 import com.wcid.practice.domain.posts.PostsRepository;
+import com.wcid.practice.web.dto.PostsListResponseDto;
 import com.wcid.practice.web.dto.PostsResponseDto;
 import com.wcid.practice.web.dto.PostsSaveRequestDto;
 import com.wcid.practice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostsService {
@@ -31,9 +35,23 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
