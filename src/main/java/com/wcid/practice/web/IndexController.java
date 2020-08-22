4194a184ec1103f7669e5298830348dd4f7e4f40
김지한 <jihankim@crowdworks.kr>
2020-08-22 21:14:19 +0900

    Google Login 동작 확인

--- src/main/java/com/wcid/practice/web/IndexController.java
+++ src/main/java/com/wcid/practice/web/IndexController.java
@@ -1,5 +1,8 @@
 package com.wcid.practice.web;
 
+import com.wcid.practice.config.auth.LoginUser;
+import com.wcid.practice.config.auth.dto.SessionUser;
+import com.wcid.practice.domain.user.User;
 import com.wcid.practice.service.PostsService;
 import com.wcid.practice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +11,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
